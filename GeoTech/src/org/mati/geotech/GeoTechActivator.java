4de05faca55d103a326aec89defe040e30e24e5c
shekochi <shekochi@016c59a1-42b3-4b4f-8cea-ddab12647373>
2009-11-28 11:18:04 +0000

    small step to Eclipse RCP

---
+++ GeoTech/src/org/mati/geotech/GeoTechActivator.java
@@ -0,0 +1,10 @@
+package org.mati.geotech;
+
+import org.eclipse.core.runtime.Plugin;
+
+public class GeoTechActivator extends Plugin {
+
+	public GeoTechActivator() {
+	}
+
+}
---
+++ GeoTech/src/org/mati/geotech/app/GeoTechApplication.java
@@ -0,0 +1,18 @@
+package org.mati.geotech.app;
+
+import org.eclipse.equinox.app.IApplication;
+import org.eclipse.equinox.app.IApplicationContext;
+
+public class GeoTechApplication implements IApplication {
+
+	@Override
+	public Object start(IApplicationContext context) throws Exception {
+		return null;
+	}
+
+	@Override
+	public void stop() {
+		
+	}
+
+}
